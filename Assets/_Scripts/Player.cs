37a84f82fb987198eb390b83ecebc4de07a95f11
Gad3lan <alan.adamiak@gmail.com>
2021-02-13 11:21:37 +0100

    Ajout joueur

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,17 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class Enemie : MonoBehaviour
+[Serializable]
+public struct Attaque
 {
+    public int degat;
+    public Button bouton;
+
+    private Attaque((int degat, Button bouton) paire)
+    {
+        this.degat = paire.degat;
+        this.bouton = paire.bouton;
+    }
+
+    public static implicit operator Attaque((int degat, Button bouton) paire)
+    {
+        return new Attaque(paire);
+    }
+}
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public List<Attaque> attacks;
+
+    public Enemie enemie;
+
     public int vie = 10;
     private int vieActueille;
     private Gradient gradient = new Gradient();
     public Slider barreDeVie;
-
-    public Player player;
 
     public void subitDegats(int degats)
     {
@@ -25,18 +46,10 @@ public class Enemie : MonoBehaviour
         {
             barreDeVie.value = (1f * vieActueille) / vie;
             barreDeVie.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color =
-            gradient.Evaluate(1f * vieActueille / vie);
+                gradient.Evaluate(1f * vieActueille / vie);
         }
-        attaque();
     }
 
-    public void attaque()
-    {
-        int degats = Random.Range(0, 5);
-        Debug.Log(degats);
-        player.subitDegats(degats);
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -50,11 +63,18 @@ public class Enemie : MonoBehaviour
         colorKey[2].color = Color.green;
         colorKey[2].time = 1.0f;
         gradient.colorKeys = colorKey;
+        foreach (var attack in attacks)
+        {
+            attack.bouton.onClick.AddListener(() =>
+            {
+                enemie.subitDegats(attack.degat);
+            });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
 }
